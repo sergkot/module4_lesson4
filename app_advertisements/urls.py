6f0b927784bbccd4364a545ecffa6c574072c3d3
sergkot <sergkot@bk.ru>
2023-07-06 13:43:14 +0300

    3pages work commit

--- app_advertisements/urls.py
+++ app_advertisements/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, test_page
 
 urlpatterns = [
-    path('user/', index)
+    path('user/', index),
+    path('test_page/', test_page)
 ]
--- app_advertisements/views.py
+++ app_advertisements/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import glob
+import os
+
 
 def index(request):
     return HttpResponse('Успешно')
+
+def test_page(request):
+    return render(request, 'test_page_static.html')
+    # mylist = [f for f in glob.glob("*.*")]
+    # return render(request, 'test_page_static.html')
+    # mylist = [f for f in glob.glob("*.*")]
+    # print(mylist)
+    #
+    # return HttpResponse('files: ' + str(mylist))
+
+    # file_location = 'test_page.html'
+    # with open(file_location, 'r') as f:
+    #     file_data = f.read()
+    # print(file_data)
+    # return HttpResponse(file_data, content_type='html')
 # Create your views here.
