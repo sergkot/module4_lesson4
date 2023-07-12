274cfa7416dd01010752b3293c28580660344e06
sergkot <sergkot@bk.ru>
2023-07-12 12:48:34 +0300

    add url link to main and top sellers

--- app_advertisements/urls.py
+++ app_advertisements/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, test_page, top_sellers
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main-page'),
     path('test_page/', test_page),
-    path('top-sellers/', top_sellers)
+    path('top-sellers/', top_sellers, name='top-sellers')
 ]
