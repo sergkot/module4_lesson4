1a5cf6dc2e228c7c6ab8781cb809c7e9eb5e320c
sergkot <sergkot@bk.ru>
2023-07-31 15:48:06 +0300

    работа с models

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisements(models.Model):
+    title = models.CharField("Заголовок",max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=17, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    test_column = models.CharField("test", max_length=100, default='')
+
+
+    def __str__(self):
+        return(f"id={self.id}, title={self.title}, description={self.description}, price={self.price}")
+
+
+    class Meta:
+        db_table = "advertisements"
