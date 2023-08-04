beff02b9c5a079848357b3cfd178011196be8c16
sergkot <sergkot@bk.ru>
2023-08-04 07:50:51 +0300

    урок 8, аминка

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import  Advertisements
+
+
+class AdvertisementsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'update_date', 'auction']
+    list_filter = ['auction', 'created_at']
+
+
+admin.site.register(Advertisements, AdvertisementsAdmin)
 
 # Register your models here.
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Advertisements(models.Model):
@@ -14,6 +15,29 @@ class Advertisements(models.Model):
     def __str__(self):
         return(f"id={self.id}, title={self.title}, description={self.description}, price={self.price}")
 
-
     class Meta:
         db_table = "advertisements"
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold">'
+                'Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def update_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.update_at.date() == timezone.now().date():
+            updated_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold">'
+                'Сегодня в {}</span>', updated_time
+            )
+        return self.update_at.strftime('%d.%m.%Y в %H:%M:%S')
