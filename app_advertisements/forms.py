75b7993d0cddaf8784b9c1af03d6e476792f5331
sergkot <sergkot@bk.ru>
2023-08-22 11:09:44 +0300

    Урок 10, формы, + детальная страница advertisment - db.sqlite

--- app_advertisements/forms.py
+++ app_advertisements/forms.py
@@ -4,13 +4,13 @@ from django.core.exceptions import ValidationError
 
 class AdvertisementsForm(forms.Form):
     title = forms.CharField(max_length=128,
-        widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+                            widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
     description = forms.CharField(
         widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
     price = forms.DecimalField(
         widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
     auction = forms.BooleanField(required=False,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+                                 widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
     # user = forms.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
     image = forms.ImageField(
         widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
